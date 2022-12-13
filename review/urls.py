2b55254a6927b8d175b4585092b8b47673b169c2
Nuraiym0 <nuraiymmamytova0@gmail.com>
2022-12-13 10:53:32 +0600

    create rating view

--- review/urls.py
+++ review/urls.py
@@ -1,16 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CommentViewSet, RetingViewSet
+from .views import CommentViewSet, CreateRatingAPIView
 
 
 router = DefaultRouter()
 router.register('comments', CommentViewSet)
-router.register('ratings', RetingViewSet)
+# router.register('ratings', CreateRatingAPIView)
 
 
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('rating/', CreateRatingAPIView.as_view())
 ]
 
 
--- review/views.py
+++ review/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 # from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .serializers import CommentSerializer, RetingSerializer
 from .models import Comment, Reting
@@ -10,11 +13,30 @@ class CommentViewSet(ModelViewSet):
     permission_classes = [IsAuthorOrReadOnly]
 
 
-class RetingViewSet(ModelViewSet):
-    queryset = Reting.objects.all()
-    serializer_class = RetingSerializer
-    permission_classes = [IsAuthorOrReadOnly]
+# class RetingViewSet(ModelViewSet):
+#     queryset = Reting.objects.all()
+#     serializer_class = RetingSerializer
+#     permission_classes = [IsAuthorOrReadOnly]
 
+
+class CreateRatingAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        user = request.user
+        ser = RetingSerializer(data=request.data, context = {'request':request})
+        ser.is_valid(raise_exception=True)
+        product_id = request.data.get('product')
+        if Reting.objects.filter(author=user, product__id=product_id).exists():
+            rating = Reting.objects.get(author=user, product__id=product_id)
+            rating.value = request.data.get('value')
+            rating.save()
+        else:
+            # Reting.objects.create(user)
+            ser.save()
+        return Response (status=201)
+
+        
 
 
 
